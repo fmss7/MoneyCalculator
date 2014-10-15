e326eab947ff16c7ec6667bf9307dddecaa3e7d3
usuario <usuario@10.22.143.10>
2014-10-15 12:24:39 +0100

    first commit 15/10/14

---
+++ src/MoneyCalculator/Money.java
@@ -0,0 +1,11 @@
+package MoneyCalculator;
+
+public class Money {
+    private int amount;
+    Currency currency = new Currency;
+
+    public Money(int amount) {
+        this.amount = amount;
+    }
+
+}
